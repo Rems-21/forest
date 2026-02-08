9daab7a075ce3bed6715aa5ebc7cb253b56924fd
Dr remus <dsonkouatremus@gmail.com>
2026-02-08 17:55:52 +0100

    fix hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -15,6 +17,11 @@ export default defineConfig(({ mode }) => {
             changeOrigin: true,
             secure: false,
           },
+        },
+      },
+      css: {
+        postcss: {
+          plugins: [tailwindcss, autoprefixer],
         },
       },
       build: {
